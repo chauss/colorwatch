4efe843729d9fed973c38bdd0714d31ba4d1054d
Christopher <Christopher@Chris_Laptop.fritz.box>
2015-04-05 11:31:54 +0200

    first commit

---
+++ Colorwatch/src/com/chhauss/colorwatch/MainActivity.java
@@ -0,0 +1,71 @@
+package com.chhauss.colorwatch;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import com.example.chhauss.colorwatch.R;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    private boolean active;
+    private View clockView;
+    private TextView clockString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        clockView = getWindow().getDecorView().getRootView();
+        
+        clockString = (TextView) findViewById(R.id.timeCodeTW);
+        clockString.setTextColor(Color.WHITE);
+        TextView clock = (TextView) findViewById(R.id.textClock);
+        clock.setTextColor(Color.WHITE);
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.active = true;
+        BackgroundAdjuster ba = new BackgroundAdjuster();
+        ba.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.active = false;
+    }
+
+    class BackgroundAdjuster extends Thread {
+
+        @Override
+        public void run() {
+            super.run();
+            SimpleDateFormat sdf = new SimpleDateFormat("HHmmss", Locale.GERMANY);
+            
+            while(active) {
+                try {
+                    Calendar cal = Calendar.getInstance(Locale.GERMANY);
+                    String colorString = "#" + sdf.format(cal.getTime());
+                    System.out.println("setting background:"+colorString);
+                    clockView.setBackgroundColor(Color.parseColor(colorString));
+                    System.out.println("setting text code");
+                    clockString.setText(colorString);
+                    System.out.println("Sleeping for 1 s");
+                    Thread.sleep(1000);
+                } catch (Exception e) { 
+                    System.out.println(e);
+                }
+            }
+        }
+        
+    }
+}
